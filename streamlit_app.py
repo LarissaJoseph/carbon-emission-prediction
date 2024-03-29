e323950a3fdf4d6edf9c13e0910a3e4575c6d659
shawnburris98 <162818125+shawnburris98@users.noreply.github.com>
2024-03-29 18:37:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ from pathlib import Path
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Carbon Emission Predictor',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_icon=':earth_americas:',	
+)# This is an emoji shortcode. Could be a URL too.
 
 	st.title('Carbon Emission Predictor App')
 
