1794f0ea99581bc244e6afbb8b367e80ce4f7b96
LarissaJoseph <163662180+LarissaJoseph@users.noreply.github.com>
2024-04-02 23:35:22 +0000

    7:35

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,15 @@ def run():
       st.subheader('Raw data')
       csv_url = './Carbon_Emission.csv'
       df = pd.read_csv(csv_url)
-      st.write(df)
+      
 
 
-      status = st.radio("Select Gender: ", ('Male', 'Female'))
+      Sex = st.radio("Select Gender: ", ('Male', 'Female'))
 
       # conditional statement to print 
       # Male if male is selected else print female
       # show the result using the success function
-      if (status == 'Male'):
+      if (Sex == 'Male'):
             st.success("Male")
       else:
             st.success("Female")
@@ -216,20 +216,22 @@ def run():
 
       # Encode(changing categorical values to numerical values)
       #df['recycling_encode'] = LabelEncoder().fit_transform(df['Recycling'])
-      df['travelingByAir_encode'] = LabelEncoder().fit_transform(df['Frequency of Traveling by Air'])
-      df['howOftenShower_encode'] = LabelEncoder().fit_transform(df['How Often Shower'])
-      df['heating_encode'] = LabelEncoder().fit_transform(df['Heating Energy Source'])
-      df['bodytype_encode'] = LabelEncoder().fit_transform(df['Body Type'])
+      df['travelingByAir_encode'] = LabelEncoder().fit_transform(df['Air'])
+      df['howOftenShower_encode'] = LabelEncoder().fit_transform(df['How_often_shower'])
+      df['heating_encode'] = LabelEncoder().fit_transform(df['Heating'])
+      df['bodytype_encode'] = LabelEncoder().fit_transform(df['Body_Type'])
       df['sex_encode'] = LabelEncoder().fit_transform(df['Sex'])
       df['diet_encode'] = LabelEncoder().fit_transform(df['Diet'])
-      df['transport_encode'] = LabelEncoder().fit_transform(df['Transport'])
-      df['socialAct_encode'] = LabelEncoder().fit_transform(df['Social Activity'])
-      df['energyEfficiency_encode'] = LabelEncoder().fit_transform(df['Energy efficiency'])
-      df['wasteBag_encode'] = LabelEncoder().fit_transform(df['Waste Bag Size'])
+      df['transport_encode'] = LabelEncoder().fit_transform(df['Trans'])
+      df['socialAct_encode'] = LabelEncoder().fit_transform(df['Social'])
+      df['energyEfficiency_encode'] = LabelEncoder().fit_transform(df['EnergyEff'])
+      df['wasteBag_encode'] = LabelEncoder().fit_transform(df['Waste_Size'])
       #df['cookingWith_encode'] = LabelEncoder().fit_transform(df['Cooking_With'])
       # Transform the 'carbon emission' variable using Box-Cox transformation
       df['carbonEmission_transform'] = stats.boxcox(df['CarbonEmission'])[0]
 
+     
+     
       # Define X (features) and y (target) and remove duplicate features that will not be used in the model
       X = df.drop(['Body Type', 'Sex', 'Diet', 'How Often Shower', 'Heating Energy Source',
             'Transport', 'Social Activity',
@@ -243,112 +245,14 @@ def run():
 
       # Instantiate a linear regression model
       linear_model = LinearRegression()
-      # Fit the model using the training data
       linear_model.fit(X_train, y_train)
-      # For each record in the test set, predict the y value (transformed value of charges)
-      # The predicted values are stored in the y_pred array
       y_pred = linear_model.predict(X_test)
 
 
       st.write('Predict your own Carbon Emission')
 
-      #Body Type
-      if  Body_Type== "underweight":
-            bodytype_encode = 3
-      elif Body_Type== "normal":
-            bodytype_encode = 0
-      elif Body_Type== "overweight":
-            bodytype_encode = 2
-      #for obese
-      else:
-            bodytype_encode = 1
 
-      #Sex    
-      if status == "Male":
-            sex_encode=1
-      else:
-            sex_encode=0
-
-      #Diet
-      if Diet == "Omnivore":
-            diet_encode = 0
-      elif Diet == "Pescatarian":
-            diet_encode = 1
-      elif Diet == "Vegetarian":
-            diet_encode = 3
-      #vegan
-      else:
-            diet_encode = 2
-
-      if How_often_shower == "Daily":
-            howOftenShower_encode = 0
-      elif How_often_shower == "Less Frequently":
-            howOftenShower_encode = 1
-      elif How_often_shower == "More Frequently":
-            howOftenShower_encode = 2
-      #twice a day
-      else:
-            howOftenShower_encode = 3
-      #heating source
-      if Heating == "Coal":
-            heating_encode = 0
-      elif Heating == "Natural Gas":
-            heating_encode = 2
-      elif Heating == "Wood":
-            heating_encode = 3
-      #elecricity 
-      else:
-            heating_encode = 1
-      #Transport
-      if Trans == "Public":
-            transport_encode = 1
-      elif Trans == "Walk/Bicycle":
-            transport_encode = 2
-      #private
-      else:
-            transport_encode = 0
-
-      #Social activity
-      if Social == "Often":
-            socialAct_encode = 1
-      elif Social == "Never":
-            socialAct_encode = 0
-      #sometimes
-            socialAct_encode = 2
-
-      #Frequency of traveling by air
-      if Air == "Frequently":
-            travelingByAir_encode = 0
-      elif Air == "Rarely":
-            travelingByAir_encode = 2
-      elif Air == "Never":
-            travelingByAir_encode = 1
-      #very frequently
-      else:
-            travelingByAir_encode = 3
-
-      #waste bag size 
-      if Waste == "Large":
-            wasteBag_encode = 1
-      elif Waste == "Extra Large":
-            wasteBag_encode = 0
-      elif Waste == "Small":
-            wasteBag_encode = 3
-      #medium
-      else:
-            wasteBag_encode = 2
-
-      #energy efficiency
-      if EnergyEff == "No":
-            energyEfficiency_encode = 0
-      elif EnergyEff == "Sometimes":
-            energyEfficiency_encode = 1
-      #yes
-      else:
-            energyEfficiency_encode = 2
-
-
-
+      
       predicted_carbon_emission_transformed = linear_model.predict([['Monthly Grocery Bill',
             'Vehicle Monthly Distance Km',
             'Waste Bag Weekly Count', 'How Long TV PC Daily Hour',
@@ -363,5 +267,5 @@ def run():
       st.write('Predicted Carbon Emission: ',round(predicted_CE[0], 0))
 
 if __name__== "__main__":
-            run()
+           run()
 
