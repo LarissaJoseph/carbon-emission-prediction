b65652409ddd3b789bfbd684c967765315edceb1
LarissaJoseph <163662180+LarissaJoseph@users.noreply.github.com>
2024-04-04 21:44:49 +0000

    finally good

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def run():
       st.set_page_config(
       page_title='Carbon Emission Predictor',
       page_icon=':earth_americas:',	
-      )# This is an emoji shortcode. Could be a URL too.
+      )
       st.write('# Carbon Emission Predictor App')
       st.subheader('Predict your own Carbon Emission')
       csv_url = './Carbon_Emission.csv'
@@ -31,10 +31,6 @@ def run():
 
 
       Sex = st.radio("Select Gender: ", ('Male', 'Female'))
-
-      # conditional statement to print 
-      # Male if male is selected else print female
-      # show the result using the success function
       if (Sex == 'Male'):
             st.success("Male")
       else:
@@ -42,165 +38,117 @@ def run():
 
       st.write("--------------------------------------------------------------------------")
 
-      # Selection box
 
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
       Body_Type = st.selectbox("Body Type: ",
                                     ['Underweight', 'Normal', 'Overweight', 'Obese'])
 
-      # print the selected hobby
+    
       st.write("Body Type: ", Body_Type)
-      # Selection box
+      
 
       st.write("--------------------------------------------------------------------------")
 
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+
       Diet = st.selectbox("Diet: ",
                                     ['Omnivore', 'Vegetarian', 'Pescatarian', 'Vegan'])
 
-      # print the selected hobby
+      
       st.write("Diet: ", Diet)
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+      
       How_often_shower = st.selectbox("How Often Do You Shower?: ",
                                     ['Daily', 'Less Frequently', 'More Frequently', 'Twice A Day'])
 
-      # print the selected hobby
+      
       st.write("How Often Do You Shower?: ", How_often_shower)
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+     
       Heating = st.selectbox("Your Home Heating Source: ",
                                     ['Coal', 'Natural Gas', 'Wood', 'Electricity'])
 
-      # print the selected hobby
       st.write("Your Home Heating Source: ", Heating)
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+      
       Trans = st.selectbox("What Is Your Main Means of Transportation?: ",
                                     ['Public', 'Walk/Bicycle', 'Private'])
 
-      # print the selected hobby
+      
       st.write("What Is Your Main Means Of Transport?: ", Trans)
-      # Selection box
+     
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+      
       Social = st.selectbox("How Often Do You Participate In Social Activities?: ",
                                     ['Often', 'Never', 'Sometimes'])
 
-      # print the selected hobby
+     
       st.write("How Often Do You Participate In Social Activities?: ", Social)
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+      
       Waste_Size = st.selectbox("Waste Bag Size: ",
                                     ['Small', 'Medium', 'Large', 'Extra Large'])
 
-      # print the selected hobby
+      
       st.write("Your Home Primiarily uses : ", Waste_Size)
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+      
       Air = st.selectbox("Frequency of using aircraft in the last month.: ",
                                     ['Frequently', 'Rarely', 'Never', 'Very Frequently'])
 
-      # print the selected hobby
+      
       st.write("Frequency of using aircraft in the last month.: ", Air )
-      # slider
+     
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+      
       Car_Dist = st.slider("Montly Distance(km) By car ", 0, 9999)
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
+     
       st.text('Selected: {}KM'.format(Car_Dist))
       st.write("--------------------------------------------------------------------------")
-      # slider
-
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+      
       Waste = st.slider("Waste Bags Weekly", 1, 7)
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
+      
       st.text('Selected: {}'.format(Waste))
-      # slider
+  
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+     
       How_longtv = st.slider("Number Of Daily Hours You Spend Infront Of The TV or Monitor", 1, 24)
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
+      
       st.text('Selected: {}Hours'.format(How_longtv))
-      # slider
+    
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+      
       Newclothes = st.slider("New Clothes Montly", 0,50 )
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
+  
       st.text('Selected: {}'.format(Newclothes))
-      # slider
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+   
       Internet = st.slider("Daily Hours Spent On Internet", 0, 24)
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
       st.text('Selected: {} Hours'.format(Internet))
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
-      # first argument takes the titleof the selectionbox
-      # second argument takes options
+    
       EnergyEff = st.selectbox("Are Your Purchases Energy Efficient? ",
                                     ['Yes', 'No', 'Sometimes'])
 
-      # print the selected hobby
       st.write("You Are Energy Aware", EnergyEff) 
-      # Selection box
+      
       st.write("--------------------------------------------------------------------------")
 
-      # slider
-
-      # first argument takes the title of the slider
-      # second argument takes the starting of the slider
-      # last argument takes the end number
+      
       Bill = st.slider("Montly Grocery Bill", 50, 299)
 
-      # print the level
-      # format() is used to print value 
-      # of a variable at a specific position
       st.text('Selected: {}$'.format(Bill), )
 
-
-      #deleting the column that has missing values
-      #df. __delitem__('Vehicle Type')
-      #df. __delitem__('Recycling')
-      #df. __delitem__('Cooking_With')
 
       df = df.drop(['Vehicle Type', 'Recycling', 'Cooking_With'], axis=1)
       
@@ -217,16 +165,7 @@ def run():
       df['wasteBag_encode'] = LabelEncoder().fit_transform(df['Waste Bag Size'])
       df['carbonEmission_transform'] = stats.boxcox(df['CarbonEmission'])[0]
 
-      #travelingByAir_encode= LabelEncoder().fit_transform([Air])[0]
-      #howOftenShower_encode= LabelEncoder().fit_transform([How_often_shower])[0]
-      #heating_encode= LabelEncoder().fit_transform([Heating])[0]
-      #bodytype_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #sex_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #diet_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #transport_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #socialAct_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #energyEfficiency_encode= LabelEncoder().fit_transform([Body_Type])[0]
-      #wasteBag_encode= LabelEncoder().fit_transform([Body_Type])[0]
+     
 
       #Frequency of traveling by air
       if Air == "Frequently":
@@ -328,21 +267,6 @@ def run():
       else:
             wasteBag_encode = 2
 
-      
-
-
-      
-      #input_data = [[Bill, Car_Dist, Waste, How_longtv, Newclothes, Internet,
-                  #LabelEncoder().fit_transform([Air])[0],
-                   #LabelEncoder().fit_transform([How_often_shower])[0],
-                   #LabelEncoder().fit_transform([Heating])[0],
-                   #LabelEncoder().fit_transform([Body_Type])[0],
-                   #LabelEncoder().fit_transform([Sex])[0],
-                   #LabelEncoder().fit_transform([Diet])[0],
-                   #LabelEncoder().fit_transform([Trans])[0],
-                   #LabelEncoder().fit_transform([Social])[0],
-                   #LabelEncoder().fit_transform([EnergyEff])[0],
-                   #LabelEncoder().fit_transform([Waste_Size])[0]]]
       
       input_data = [[Bill, Car_Dist, Waste, How_longtv, Newclothes, Internet,
                   travelingByAir_encode,howOftenShower_encode,heating_encode,bodytype_encode,
